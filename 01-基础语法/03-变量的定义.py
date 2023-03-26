9762f5a761bd4510b4787a982f43638880f75bca
VunboYao <vunbo@outlook.com>
2023-03-27 00:48:29 +0800

    feat: Python basic

---
+++ 01-基础语法/03-变量的定义.py
@@ -0,0 +1,5 @@
+balance = 50
+print('余额:', balance)
+
+balance += 20
+print('New Price:', balance)
---
+++ 01-基础语法/04-数据类型.py
@@ -0,0 +1,11 @@
+"""
+    type(数据)，返回数据的类型
+"""
+print(type(666))  # int
+print(type(13.14))  # float
+print(type('Hello'))  # str
+print(type(True))  # bool
+print(type(4+3j))  # complex
+print(type((12, 12)))  # tuple
+print(type({}))  # dict
+print(type(set([1, 3])))  # set
